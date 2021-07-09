fd595d323a20eda43b1bbf220dea5fee00d4b2b6
dmdylan <51095343+dmdylan@users.noreply.github.com>
2021-07-09 18:07:46 -0400

    Character movement mostly works. Added in animation files.

--- Assets/Project/Runtime/Scripts/PlayerLocomotion.cs
+++ Assets/Project/Runtime/Scripts/PlayerLocomotion.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using Mirror;
 
+
+//TODO: Need to set vcamera follow through script?
 public class PlayerLocomotion : NetworkBehaviour
 {
+    Camera playerCamera;
     Rigidbody playerBody;
+    [SerializeField] GameObject playerModel;
     [SerializeField] private float m_Speed = 5f;
     [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float rotationSpeed = 5f;
     private PlayerInput playerInput;
     private InputAction movementAction;
     private InputAction jumpAction;
-    private Vector3 currentMovementInput;
+    private Vector2 currentMovementInput;
     private Vector3 currentMovement;
     private bool isMovementPressed;
     private bool isGrounded;
@@ -20,6 +25,9 @@ public class PlayerLocomotion : NetworkBehaviour
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask groundMask;
     [SerializeField] float groundDistance = 0.2f;
+    private float turnSpeedMultiplier;
+    private Vector3 targetDirection;
+    private Quaternion freeRotation;
 
     private void OnEnable()
     {
@@ -36,7 +44,8 @@ public class PlayerLocomotion : NetworkBehaviour
     private void Awake()
     {
         playerBody = GetComponent<Rigidbody>();
-        playerInput = GetComponent<PlayerInput>(); 
+        playerInput = GetComponent<PlayerInput>();
+        playerCamera = Camera.main;
     }
 
     void Start()
@@ -49,9 +58,14 @@ public class PlayerLocomotion : NetworkBehaviour
         jumpAction.performed += JumpAction_performed;
     }
 
+    //TODO: Pretty sure it jitters when jumping because currentMovement.y is set to 0 each frame
+    //Need to read and cache before jumping
     private void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        currentMovement = new Vector3(currentMovementInput.x, 0, currentMovementInput.y);
+        currentMovement = currentMovement.x * playerCamera.transform.right.normalized + currentMovement.z * playerCamera.transform.forward.normalized;
+        currentMovement.y = 0;
     }
 
     void FixedUpdate()
@@ -59,13 +73,38 @@ public class PlayerLocomotion : NetworkBehaviour
         //Apply the movement vector to the current position, which is
         //multiplied by deltaTime and speed for a smooth MovePosition
         playerBody.MovePosition(transform.position + currentMovement * Time.deltaTime * m_Speed);
+        TargetDirection();
+        if (currentMovementInput != Vector2.zero && targetDirection.magnitude > 0.1f)
+        {
+            Vector3 lookDirection = targetDirection.normalized;
+            freeRotation = Quaternion.LookRotation(lookDirection, transform.up);
+            var diferenceRotation = freeRotation.eulerAngles.y - transform.eulerAngles.y;
+            var eulerY = transform.eulerAngles.y;
+
+            if (diferenceRotation < 0 || diferenceRotation > 0) eulerY = freeRotation.eulerAngles.y;
+            var euler = new Vector3(0, eulerY, 0);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(euler), rotationSpeed * turnSpeedMultiplier * Time.deltaTime);
+        }
+    }
+
+    private void TargetDirection()
+    {
+        turnSpeedMultiplier = 1f;
+        var forward = playerCamera.transform.TransformDirection(Vector3.forward);
+        forward.y = 0;
+
+        //get the right-facing direction of the referenceTransform
+        var right = playerCamera.transform.TransformDirection(Vector3.right);
+
+        // determine the direction the player will face based on input and the referenceTransform's right and forward directions
+        targetDirection = currentMovementInput.x * right + currentMovementInput.y * forward;
     }
 
     private void Movement_performed(InputAction.CallbackContext context)
     {
         currentMovementInput = context.ReadValue<Vector2>();
-        currentMovement.x = currentMovementInput.x;
-        currentMovement.z = currentMovementInput.y;
+
         isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
     }
 
