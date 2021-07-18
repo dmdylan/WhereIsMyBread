7f5f0b15ac1e518872732bf18d26beca3ee4063e
dmdylan <51095343+dmdylan@users.noreply.github.com>
2021-07-18 18:19:59 -0400

    Adding aim camera and fixing input scripts

--- Assets/Project/Runtime/Scripts/ActivateOnKeypress.cs
+++ Assets/Project/Runtime/Scripts/ActivateOnKeypress.cs
@@ -1,24 +1,49 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ActivateOnKeypress : MonoBehaviour
 {
-    public KeyCode ActivationKey = KeyCode.LeftControl;
+    private PlayerInput playerInput;
+    private InputAction aimAction;
     public int PriorityBoostAmount = 10;
     public GameObject Reticle;
 
     Cinemachine.CinemachineVirtualCameraBase vcam;
     bool boosted = false;
 
+    private void OnEnable()
+    {
+        playerInput.actions["Aim"].Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.actions["Aim"].Disable();
+    }
+
     void Start()
     {
+        playerInput = GetComponent<PlayerInput>();
         vcam = GetComponent<Cinemachine.CinemachineVirtualCameraBase>();
+        aimAction.started += AimAction_started;
+        aimAction.canceled += AimAction_canceled;
+    }
+
+    private void AimAction_canceled(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void AimAction_started(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
     }
 
     void Update()
     {
         if (vcam != null)
         {
-            if (Input.GetKey(ActivationKey))
+            if (Input.GetKey(KeyCode.Mouse1))
             {
                 if (!boosted)
                 {
