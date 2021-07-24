9bf29dd14a32ee95dc2fe0d06355d83fc6dc3b92
dmdylan <51095343+dmdylan@users.noreply.github.com>
2021-07-24 19:45:36 -0400

    Added aim movement and camera.

--- Assets/Project/Runtime/Input/StarterAssetsInputs.cs
+++ Assets/Project/Runtime/Input/StarterAssetsInputs.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 using UnityEngine.InputSystem;
@@ -7,23 +8,30 @@ namespace StarterAssets
 {
 	public class StarterAssetsInputs : MonoBehaviour
 	{
-		[Header("Character Input Values")]
-		public Vector2 move;
-		public Vector2 look;
-		public bool jump;
-		public bool sprint;
+        [Header("Character Input Values")]
+        private Vector2 move;
+        private Vector2 look;
+        private bool jump;
+        private bool sprint;
+		private bool aim;
 
-		[Header("Movement Settings")]
+        [Header("Movement Settings")]
 		public bool analogMovement;
 
 #if !UNITY_IOS || !UNITY_ANDROID
 		[Header("Mouse Cursor Settings")]
 		public bool cursorLocked = true;
 		public bool cursorInputForLook = true;
+
+		public Vector2 Move => move;
+		public Vector2 Look => look;
+		public bool Jump => jump;
+		public bool Sprint => sprint;
+		public bool Aim => aim;
 #endif
 
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
-		public void OnMove(InputValue value)
+        public void OnMove(InputValue value)
 		{
 			MoveInput(value.Get<Vector2>());
 		}
@@ -38,13 +46,18 @@ namespace StarterAssets
 
 		public void OnJump(InputValue value)
 		{
-			JumpInput(value.isPressed);
-		}
+            StartCoroutine(JumpInput(value.isPressed));
+        }
 
 		public void OnSprint(InputValue value)
 		{
 			SprintInput(value.isPressed);
 		}
+
+		public void OnAim(InputValue value)
+        {
+			AimInput(value.isPressed);
+        }
 #else
 	// old input sys if we do decide to have it (most likely wont)...
 #endif
@@ -60,15 +73,22 @@ namespace StarterAssets
 			look = newLookDirection;
 		}
 
-		public void JumpInput(bool newJumpState)
+		IEnumerator JumpInput(bool newJumpState)
 		{
 			jump = newJumpState;
+			yield return new WaitForSeconds(.1f);
+			jump = !jump;
 		}
 
 		public void SprintInput(bool newSprintState)
 		{
 			sprint = newSprintState;
 		}
+
+		public void AimInput(bool newAimState)
+        {
+			aim = newAimState;
+        }
 
 #if !UNITY_IOS || !UNITY_ANDROID
 
--- Assets/Project/Runtime/Scripts/AimCameraScript.cs
+++ Assets/Project/Runtime/Scripts/AimCameraScript.cs
@@ -1,58 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
 using Cinemachine;
+using StarterAssets;
 
 public class AimCameraScript : MonoBehaviour
 {
-    [SerializeField] private GameObject reticle;
-    private PlayerInput playerInput;
-    private InputAction aimAction;
-    private int priorityBoostAmount = 10;
-    private CinemachineVirtualCameraBase vcam;
-    private bool boosted = false;
-
-    private void OnEnable()
-    {
-        playerInput.actions["Aim"].Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerInput.actions["Aim"].Disable();
-    }
-
-    private void Awake()
-    {
-        playerInput = GetComponent<PlayerInput>();
-    }
+    //[SerializeField] private GameObject reticle;
+    private CinemachineVirtualCamera vcam;
+    private StarterAssetsInputs input;
 
     void Start()
     {
         vcam = GameManager.Instance.CinemachineVirtualCameras[1];
-        aimAction = playerInput.actions["Aim"];
-        aimAction.started += AimAction_started;
-        aimAction.canceled += AimAction_canceled;
+        input = GetComponent<StarterAssetsInputs>();
     }
 
-    private void AimAction_canceled(InputAction.CallbackContext context)
+    private void OnAim()
     {
-        if (boosted)
-        {         
-            vcam.Priority -= priorityBoostAmount;
-            boosted = false;
-        }
-        if (reticle != null)
-            reticle.SetActive(boosted);
-    }
-
-    private void AimAction_started(InputAction.CallbackContext context)
-    {
-        if (!boosted)
-        {
-            vcam.Priority += priorityBoostAmount;
-            boosted = true;
-        }
-        if (reticle != null)
-            reticle.SetActive(boosted);
+        vcam.Priority += 10;
     }
 }
