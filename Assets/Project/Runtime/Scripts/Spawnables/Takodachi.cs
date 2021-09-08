998a6c77f2daf4d875d39874195a1777cfc3e699
dmdylan <51095343+dmdylan@users.noreply.github.com>
2021-09-07 23:29:38 -0400

    Tako fixed. Working on spawn location bug.

--- Assets/Project/Runtime/Scripts/Spawnables/Takodachi.cs
+++ Assets/Project/Runtime/Scripts/Spawnables/Takodachi.cs
@@ -1,8 +1,8 @@
 using Mirror;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using BreadStuff;
+using DG.Tweening;
 
 public class Takodachi : NetworkBehaviour
 {
@@ -19,12 +19,20 @@ public class Takodachi : NetworkBehaviour
         if(collision.gameObject.TryGetComponent(out Bread bread))
         {
             bread.PlayerRigidbody.AddForce(Vector3.up * takodachiAbility.AbilityEffectFloat, ForceMode.Impulse);
+            CmdTakoTween();
         }
     }
 
     IEnumerator DestroyTheTako()
     {
         yield return new WaitForSeconds(takodachiAbility.AbilityDuration);
+        DOTween.Clear();
         NetworkServer.Destroy(gameObject);
+    }
+
+    [Command(requiresAuthority = false)]
+    void CmdTakoTween(NetworkConnectionToClient sender = null)
+    {
+        transform.DOShakeScale(.5f, 1f, 5, 30, true);
     }
 }
