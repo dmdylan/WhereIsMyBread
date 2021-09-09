af2e5b4b713a894fbcf810e4a999ec92e3334ffe
dmdylan <51095343+dmdylan@users.noreply.github.com>
2021-09-08 23:12:44 -0400

    Game timer working

--- Assets/Project/Runtime/Scripts/GameManager.cs
+++ Assets/Project/Runtime/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ using Mirror;
 using System.Collections;
 using TMPro;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
     static GameManager instance;
     public static GameManager Instance
@@ -21,15 +21,32 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    //TODO: Might work as SO instead of syncvar.
+    [SyncVar(hook = nameof(UpdateTimer))]
+    private int gameTimer;
+
+    [SerializeField] private int gameLength;
     [SerializeField] private Image abilityOneImage;
     [SerializeField] private Image abilityTwoImage;
     [SerializeField] private TMP_Text abilityOneCDText;
     [SerializeField] private TMP_Text abilityTwoCDText;
+    [SerializeField] private TMP_Text gameTimerText;
     [SerializeField] private CinemachineVirtualCamera[] cinemachineGuraCameras;
     [SerializeField] private CinemachineVirtualCamera[] cinemachineBreadCameras;
 
     private AbilitySO abilityOne;
     private AbilitySO abilityTwo;
+
+    private void Start()
+    {
+        gameTimer = gameLength;
+        StartCoroutine(GameTimer());
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Game over!");
+    }
 
     #region UI
 
@@ -39,6 +56,18 @@ public class GameManager : MonoBehaviour
         this.abilityTwo = abilityTwo;
         abilityOneImage.sprite = abilityOne.Icon;
         abilityTwoImage.sprite = abilityTwo.Icon;
+    }
+
+    IEnumerator GameTimer()
+    {
+        while(gameTimer > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            gameTimer--;
+        }
+
+        //TODO: Stuff when timer is 0
+        GameOver();
     }
 
     public IEnumerator AbilityOneCountdown(float duration)
@@ -91,6 +120,18 @@ public class GameManager : MonoBehaviour
     }
 
     //TODO: Get list of all players that join match. Can be used to set other players follow cameras for spectating.
+
+    #endregion
+
+    #region Hooks
+
+    void UpdateTimer(int oldValue, int newValue)
+    {
+        var minutes = newValue / 60;
+        var seconds = newValue % 60;
+
+        gameTimerText.text = $"{minutes:00} : {seconds:00}";
+    }
 
     #endregion
 }
